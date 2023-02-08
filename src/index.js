65092ae68673a9e1291475c34e6228ab6316d20c
Pamguicha <pamelaguicharrousse@gmail.com>
2023-02-08 15:32:22 +1100

    delete extra components not in use

--- src/index.js
+++ src/index.js
@@ -2,23 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <div>
+  <footer>
          <small>
-        <a className="open-source" href="https://github.com/Pamguicha/first-git" target="_blank" rel="noreferrer" >
+        <a className="open-source" href="https://github.com/Pamguicha/react-weather-app" target="_blank" rel="noreferrer" >
             Open-source code, </a>
         by Pamela Mardones
     </small>
-    </div>
+    </footer>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
