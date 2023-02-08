3b47d4d7e9f690b428e64acab7706e984a728ec7
Pamguicha <pamelaguicharrousse@gmail.com>
2023-02-08 12:20:29 +1100

    change css buttons

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
   return (
        <div className="city">
 <p className="intro"> The weather app</p>
- <form>
+ <form className="search-form">
  <input type="text" placeholder="Enter your city here" />
    <input className="btn" type="submit" value="Search" />
-        <input className="currentBtn" type="submit" value="Current" />
+        <input className="currentbar" type="button" value="Current" />
   </form>
    <Main />
       <Forecast />
